b8fd987329739c29fc340c24b1018194ef50430a
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-11 14:14:11 +0400

    solve moving north once

--- TestTDD/MarsRover.cs
+++ TestTDD/MarsRover.cs
@@ -31,14 +31,21 @@ namespace TestTDD
     {
         public static object Move(Point startPoint, char direction, string movements)
         {
-            if ((startPoint, direction, movements) == (new Point(3, 2), 'N', "f"))
-                return new Point(3, 3);
-            if ((startPoint, direction, movements) == (new Point(4, 4), 'N', "f"))
-                return new Point(4, 5);
-            if ((startPoint, direction, movements) == (new Point(7, 0), 'N', "f"))
-                return new Point(7, 1);
-            return new Point(0, 1);
+            //if ((startPoint, direction, movements) == (new Point(3, 2), 'N', "f"))
+            //    return new Point(3, 3);
+            //if ((startPoint, direction, movements) == (new Point(4, 4), 'N', "f"))
+            //    return new Point(4, 5);
+            //if ((startPoint, direction, movements) == (new Point(7, 0), 'N', "f"))
+            //    return new Point(7, 1);
+            var res = startPoint.MoveNorth();
+            return res;
         }
     }
-    public record Point(int x, int y) { }
+    public record Point(int x, int y)
+    {
+        internal Point MoveNorth()
+        {
+            return new Point(this.x, this.y+1);
+        }
+    }
 }
